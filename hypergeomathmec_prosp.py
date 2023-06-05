c31f95c76c255b792f9fed698fb75c03f7b612f9
Dullahaut <130243684+Dullahaut@users.noreply.github.com>
2023-06-04 21:36:18 -0400

    Add files via upload

---
+++ hypergeomathmec_prosp.py
@@ -0,0 +1,489 @@
+#import openpyxl
+import numpy as np
+#import csv
+"""
+manif = open('C:/Users/denca/.spyder-py3/failstates.csv', 'w', newline='')
+manifestwriter = csv.writer(manif, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+manifestwriter.writerow(['#hand1','#hand2','#hand3','#hand4','#hand5'])
+manif_currentrow = []
+"""
+
+deck = np.array(['alpha','alpha','alpha','zeta','zeta','zeta','gamma','gamma','delta','nova','nova','nova',
+        'fafnir','fafnir','fafnir','emergency','emergency','emergency','foolish','called',
+        'prep','prep','prep','jack','jack','jack',
+        'met','diviner','benten','benten','benten','natasha','draconids','pain','shot','shot','shot','ariadne',
+        'ne1','ne1','ne1',
+        'ne','ne','ne',
+        'ne','ne','ne',
+        'prosp','prosp','prosp'])
+print("deck size: %d" % len(deck))
+hand = []
+prospreveal = []
+playable = 0
+count = 0
+pper = 0.00
+nibprot = 0
+nibper = 0.00
+eyy = 0
+delbric = 0
+dbper = 0.00
+nfaf = 0
+nfafper = 0
+nfafbric = 0
+nfafbricper = 0
+necom = 0
+necomper = 0
+nonames = 0
+backups = 0
+
+a = 0
+z = 0
+g = 0
+d = 0
+names = 0
+dupenames = 0
+
+em = 0
+no = 0
+#nfaf = 0
+faf = 0
+novaih = 0
+jack = 0
+search = 0
+dupsearch = 0
+
+ben = 0
+pain = 0
+drac = 0
+nat = 0
+freerit = 0
+efb = 0
+    
+prot = 0
+a_nib = 0
+cross = 0
+drag = 0
+    
+n = 0
+nib = 0
+ash = 0
+ht = 0
+dht = 0
+    
+div = 0
+fool = 0
+freefool = 0
+met = 0
+ariadne = 0
+trap = 0
+  
+combo = 0
+    
+ne = 0
+veil = 0
+prosp = 0
+prospextra = 0
+
+def handparsing(groupsize):
+    global a
+    global z
+    global g
+    global d
+    global names
+
+    global em
+    global no
+
+    global faf
+    global novaih
+    global jack
+
+    global ben
+    global pain
+    global drac
+    global nat
+    global freerit
+
+    global a_nib
+    global cross
+ 
+    global n
+
+    global div
+    global fool
+    global met
+    global ariadne
+    global trap
+    
+    global ne
+    global veil
+    global prosp
+    
+    a = 0
+    z = 0
+    g = 0
+    d = 0
+    names = 0
+
+    em = 0
+    no = 0
+    faf = 0
+    novaih = 0
+    jack = 0
+
+    ben = 0
+    pain = 0
+    drac = 0
+    nat = 0
+    freerit = 0
+    
+    a_nib = 0
+    cross = 0
+    
+    n = 0
+    
+    div = 0
+    fool = 0
+    met = 0
+    ariadne = 0
+    trap = 0
+    
+    ne = 0
+    veil = 0
+    
+    for i in range(0,groupsize):
+        if hand[i] == 'alpha':
+            a = 1
+            names += 1
+        if hand[i] == 'zeta':
+            z = 1
+            names += 1
+        if hand[i] == 'gamma':
+            g = 1
+            names += 1
+        if hand[i] == 'delta':
+            d = 1
+            names += 1
+        if hand[i] == 'emergency':
+            em = 1
+            #search += 1
+        if hand[i] == 'nova' or hand[i] == 'fafnir' or hand[i] == 'tera':
+            no = 1
+            #nfaf +=1
+            #search += 1
+            if hand[i] == 'nova':
+                novaih += 1
+            if hand[i] == 'fafnir' or hand[i] == 'tera':
+                faf += 1
+        if hand[i] == 'foolish':
+            fool = 1
+        if hand[i] == 'benten' or hand[i] == 'prep':
+            ben += 1
+            freerit = 1
+        if hand[i] == 'pain':
+            pain += 1 
+            freerit = 1
+        if hand[i] == 'draconids':
+            drac += 1
+            freerit = 1
+        if hand[i] == 'natasha':
+            nat += 1 
+        if hand[i] == 'diviner':
+            div += 1
+        """
+        if hand[i] == 'called' or hand[i] == 'instant' or hand[i] == 'crossout' or hand[i] == 'dragged':
+            prot += 1
+            if hand[i] == 'crossout' or hand[i] == 'dragged':
+                a_nib = 1
+                cross += 1
+        if hand[i] == 'nibiru':
+            n = 1
+            nib += 1
+            ht += 1
+        if hand[i] == 'ash':
+            ash = 1
+            ht += 1
+        """
+        if hand[i] == 'met':
+            met += 1
+        if hand[i] == 'shot' or hand[i] == 'ariadne':
+            trap += 1
+        if hand[i] == 'jack':
+            jack = 1
+        """
+        if hand [i] == 'efb' and efb == 0:
+            efb = 1
+            if ben == 0:
+                ben += 1
+            elif ben > 0 and pain == 0:
+                pain == 1
+            elif pain == 1 and drac == 0:
+                drac += 1
+            elif pain == 1 and drac == 1:
+                ben += 1
+        """
+        if hand[i] == 'ne':
+            ne += 1
+        if hand[i] == 'ne1':
+            veil == 1
+            ne +=1
+        """
+        if hand[i] == 'dragged':
+            drag = 1
+        if hand[i] == 'ariadne':
+            ariadne += 1
+        """
+    
+def diagnosing():
+    global a
+    global z
+    global g
+    global d
+    global names
+
+    global em
+    global no
+    global faf
+    global novaih
+    global jack
+
+    global ben
+    global pain
+    global drac
+    global nat
+    global freerit
+    
+    global a_nib
+    global cross
+    
+    global n
+    
+    global div
+    global fool
+    global met
+    global ariadne
+    global trap
+    
+    global ne
+    global veil
+    
+    global dupenames
+    global freefool
+    global combo
+    global eyy
+    
+    combo = 0
+    a_nib = 0
+    
+    if jack == 1:
+        if z == 0:
+            z = 1
+        elif g == 0:
+            g = 1
+        elif d == 0:
+            d = 1
+        else:
+            dupenames += 1    
+    if fool == 1 and a == 0:
+        a = 1
+    elif fool == 1 and a == 1 and (dupenames > 1 or freerit >= 1):
+        freefool += 1
+    
+    if a == 1 and (z == 1 or g == 1 or d == 1 or em == 1 or no == 1 or ben > 1 or (ben == 0 and (pain ==1 or met == 1))):
+        combo += 1
+    elif (z == 1 or g == 1 or d == 1 or ben >= 1 or div == 1) and (em == 1 or no == 1):
+        combo += 1
+    elif em == 1 and no == 1:
+        combo += 1
+    elif z == 1 and (ben >= 1 or div == 1):
+        combo += 1
+    elif g == 1 and names >= 2 and ben > 0:
+        combo += 1
+    elif d == 1 and ben >= 1 and (a == 1 or z == 1 or g == 1 or em == 1 or no == 1):
+        combo += 1
+    elif d == 1 and ben >= 1 and (a == 1 or z == 1 or g == 1 or em == 1 or no == 1):
+        combo += 1
+    elif d == 1 and (a == 1 or em == 1 or no == 1) and (pain == 1 or met == 1):
+        combo += 1
+    elif d == 1 and (div == 1 or ben > 0) and (z == 1 or g == 1 or em == 1 or no == 1):
+        combo += 1
+    elif d == 1 and ((met == 1 or div == 1) and ben > 0) or (met == 1  and (ben > 0 or div == 1)):
+        combo += 1
+    elif d == 1 and z == 1 and g == 1:
+        combo += 1
+    elif d == 1 and ben > 1 and pain == 1:
+        combo += 1
+    elif no == 1 and pain == 1 and veil == 1:
+        combo += 1
+    elif fool == 1 and (div == 1 or ben > 0):
+        combo += 1
+    elif trap == 1 and (d == 1 or z == 1) and (g == 1 or jack == 1):
+        combo += 1
+    elif trap == 1 and z == 1 and g == 1:
+        combo += 1
+    elif trap == 1 and z == 1 and g == 1 and (pain == 1 or drac == 1 or nat == 1):
+        combo += 1
+    elif trap == 1 and z == 1 and g == 1 and (pain == 1 or drac == 1 or nat == 1):
+        combo += 1
+    elif trap == 1 and (met == 1 or div == 1) and pain == 1 and drac == 1:
+        combo += 1
+    elif trap == 1 and met == 1 and (pain == 1 or div == 1) and drac == 1:
+        combo += 1
+    elif trap == 1 and met == 1 and pain == 1 and (drac == 1 or div == 1):
+        combo += 1
+    elif trap == 1 and g == 1 and d == 1 and (pain == 1 or div == 1):
+        combo += 1
+    elif trap == 1 and met == 1 and d == 1 and dupenames > 0 and pain == 1:
+        combo += 1
+    elif trap == 1 and d == 1 and pain == 1 and ben > 0:
+        combo += 1
+
+    if trap == 1 and met == 1 and pain == 1 and drac == 1:
+        eyy += 1
+        
+    if (ben > 0 or pain == 1 or drac == 1 or div == 1) and (a == 1 or em == 1) and z == 1:
+        a_nib = 1
+    elif (ben > 0 or pain == 1 or drac == 1 or div == 1) and a == 1 and (z == 1 or em == 1):
+        a_nib = 1
+    elif no == 1 and ben > 0 and (a == 1 or z == 1 or em == 1):
+        a_nib = 1
+    elif ben > 0 and (((z == 1 or em == 1) and (g == 1 or d == 1)) or (z == 1 and (g == 1 or d == 1 or em == 1))):
+        a_nib = 1
+    
+def recording():
+    global playable
+    global nfaf
+    global nfafbric
+    global delbric
+    global nibprot
+    global necom
+    global nonames
+    global backups
+    
+    if combo >= 1:
+        playable += 1   
+    if d == 1 and combo == 0:
+        delbric += 1
+    if novaih + faf > 1:
+        nfaf += 1
+    if (novaih+faf>1) and combo == 0:
+        nfafbric += 1
+    if a_nib == 1 and combo >= 1:
+        nibprot += 1
+        #print('playable\n')
+    if ne > 0 and combo >= 1:
+        necom += 1
+    if combo == 0:
+        if a == 0 and z == 0 and g == 0 and d == 0 and no == 0 and em == 0:
+            nonames += 1
+        elif ((a + z + g + d + no + em) <= 0) and ((ben + drac + nat) == 0):
+            nonames += 1
+        elif ne >= 4:
+            nonames += 1
+        elif ne >= 3 and (pain == 1 or drac == 1):
+            nonames += 1
+        elif ((g == 1 or em == 1 or no == 1) and z == 1) or (g == 1 and (z == 1 or em == 1 or no == 1)):
+            backups += 1
+        elif d == 1 and ben == 1 and pain == 1:
+            backups += 1
+        elif a == 1 and ben == 1:
+            backups += 1
+        """
+        else:
+            manif_currentrow.clear()
+            for j in range(0,5):
+                manif_currentrow.append(hand[j])
+            manifestwriter.writerow(manif_currentrow)
+        """
+
+
+
+for run in range(0,100000):
+    hand = []
+    prospreveal = []
+    count += 1
+    prosp = 0
+    
+    np.random.shuffle(deck)
+
+    #print(len(deck))
+    for i in range(0,5):
+        hand.append(deck[i])
+    for i in range(5,12):
+        prospreveal.append(deck[i])
+    #print(hand)
+    handparsing(5)
+    dupenames = names - (a + z + g + d)
+    names -= dupenames
+    """
+    dupesearch = search - (em + no)
+    search -= dupesearch
+    dht = ht - (n + ash)
+    print("names: %d" % names)
+    print("dupe names: %d" % dupenames)
+    print("searchers: %d" % search)
+    print("dupe searchers: %d" % dupesearch)
+    print("protections: %d" % prot)
+    print("nib protected?: %d" % a_nib)
+    print("handtraps: %d" % ht)
+    print("dupe handtraps: %d" % dht)
+    """
+    
+    if 'prosp' in hand:
+        prosp = 1
+    
+    diagnosing()
+
+    if prosp == 1:
+        if combo == 0:
+            hand.append('blank')
+            for card in prospreveal:
+                hand[5] = card
+                handparsing(6)
+                diagnosing()
+                if a_nib == 1:
+                    break
+                if combo > 0:
+                    break
+        elif combo > 0:
+            hand.append('blank')
+            for card in prospreveal:
+                hand[5] = card
+                handparsing(6)
+                diagnosing()
+                if a_nib == 1:
+                    break
+            if a_nib == 0:
+                hand.pop(5)
+                prospextra += 1 
+        elif combo > 0 and ('ne' in prospreveal) or ('ne1' in prospreveal):
+            ne += 1
+        elif combo > 0:
+            prospextra += 1
+
+    recording()
+
+pper = (playable/count)*100
+nibper = (nibprot/count)*100
+dbper = (delbric/count)*100
+nfafper = (nfaf/count)*100
+nfafbricper = (nfafbric/count)*100
+necomper = (necom/count)*100
+nonameper = (nonames/count)*100
+backupper = (backups/count)*100
+prospextraper = (prospextra/count)*100
+"""
+print(eyy)
+print(playable)
+"""
+print("percentage playable: %d" % pper + "%" + "  %d" % playable)
+print("percentage playable through nib: %d" % nibper + "%" + "  %d" % nibprot)
+print("percentage playable with non-engine in hand: %d" % necomper + "%" + "  %d" % necom)
+print("percentage hands with zero names: %d" % nonameper + "%" + " %d" % nonames)
+print("percentage hands that must use the backup combo: %d" % backupper + "%" + " %d" % backups)
+print("percentage hands that prosp is extraneous: %d" % prospextraper + "%" + " %d" % prospextra)
+"""
+print("percentage bricks with delta: %d" % dbper +"%")
+print("percentage hands with 2 fafnir: %d" % nfafper +"%" + "  %d" % nfaf)
+print("percentage bricks with 2 fafnir: %d" % nfafbricper +"%" + "  %d" % nfafbric)
+"""
--- hypergeomathmech_azalea.py
+++ hypergeomathmech_azalea.py
@@ -1,9 +1,9 @@
 #import openpyxl
 import numpy as np
 
-deck = np.array(['alpha','alpha','alpha','zeta','zeta','zeta','gamma','gamma','delta','nova','nova','nova',
+deck = np.array(['alpha','alpha','alpha','zeta','zeta','zeta','gamma','gamma','delta','delta','nova','nova','nova',
         'fafnir','fafnir','fafnir','emergency','emergency','emergency','foolish','called',
-        'prep','prep','jack','jack','jack','efb','efb',
+        'prep','prep','prep','jack','jack','jack',
         'met','diviner','benten','benten','benten','natasha','draconids','pain','shot','shot','shot','ariadne',
         'ne','ne','ne',
         'ne','ne','ne',
@@ -24,6 +24,7 @@ nfafbric = 0
 nfafbricper = 0
 necom = 0
 necomper = 0
+nonames = 0
 
 for run in range(0,100000):
     hand = []
@@ -64,6 +65,7 @@ for run in range(0,100000):
     
     div = 0
     fool = 0
+    freefool = 0
     met = 0
     ariadne = 0
     trap = 0
@@ -96,11 +98,11 @@ for run in range(0,100000):
             names += 1
         if hand[i] == 'emergency':
             em = 1
-            search += 1
+            #search += 1
         if hand[i] == 'nova' or hand[i] == 'fafnir' or hand[i] == 'tera':
             no = 1
             #nfaf +=1
-            search += 1
+            #search += 1
             if hand[i] == 'nova':
                 novaih += 1
             if hand[i] == 'fafnir' or hand[i] == 'tera':
@@ -120,6 +122,7 @@ for run in range(0,100000):
             nat += 1 
         if hand[i] == 'diviner':
             div += 1
+        """
         if hand[i] == 'called' or hand[i] == 'instant' or hand[i] == 'crossout' or hand[i] == 'dragged':
             prot += 1
             if hand[i] == 'crossout' or hand[i] == 'dragged':
@@ -132,6 +135,7 @@ for run in range(0,100000):
         if hand[i] == 'ash':
             ash = 1
             ht += 1
+        """
         if hand[i] == 'met':
             met += 1
         if hand[i] == 'shot' or hand[i] == 'ariadne':
@@ -158,10 +162,10 @@ for run in range(0,100000):
         """
         dupenames = names - (a + z + g + d)
         names -= dupenames
+        """
         dupesearch = search - (em + no)
         search -= dupesearch
         dht = ht - (n + ash)
-        """
         print("names: %d" % names)
         print("dupe names: %d" % dupenames)
         print("searchers: %d" % search)
@@ -183,10 +187,11 @@ for run in range(0,100000):
     if fool == 1 and a == 0:
         a = 1
     elif fool == 1 and a == 1 and (dupenames > 1 or freerit >= 1):
-        search += 1
+        freefool += 1
+    
     if a == 1 and (z == 1 or g == 1 or d == 1 or em == 1 or no == 1 or ben >= 1):
         combo += 1
-    elif (z == 1 or g == 1 or d == 1 or ben >= 1 or div == 1) and search >= 1:
+    elif (z == 1 or g == 1 or d == 1 or ben >= 1 or div == 1) and (em == 1 or no == 1):
         combo += 1
     elif em == 1 and no == 1:
         combo += 1
@@ -199,6 +204,8 @@ for run in range(0,100000):
     elif d == 1 and ben >= 1 and (a == 1 or z == 1 or g == 1 or em == 1 or no == 1):
         combo += 1
     elif d == 1 and (a == 1 or em == 1 or no == 1) and (pain == 1 or met == 1):
+        combo += 1
+    elif d == 1 and (div == 1 or ben > 0) and (z == 1 or g == 1 or em == 1 or no == 1):
         combo += 1
     elif trap == 1 and z == 1 and g == 1 and (pain == 1 or drac == 1 or nat == 1):
         combo += 1
@@ -218,9 +225,9 @@ for run in range(0,100000):
     if trap == 1 and met == 1 and pain == 1 and drac == 1:
         eyy += 1
         
-    if (ben > 0 or pain == 1 or drac == 1) and (a == 1 or em == 1) and z == 1:
+    if (ben > 0 or pain == 1 or drac == 1 or div == 1) and (a == 1 or em == 1) and z == 1:
         a_nib = 1
-    elif (ben > 0 or pain == 1 or drac == 1) and a == 1 and (z == 1 or em == 1):
+    elif (ben > 0 or pain == 1 or drac == 1 or div == 1) and a == 1 and (z == 1 or em == 1):
         a_nib = 1
     elif no == 1 and ben > 0 and (a == 1 or z == 1 or em == 1):
         a_nib = 1
@@ -240,6 +247,8 @@ for run in range(0,100000):
         #print('playable\n')
     if ne > 0 and combo >= 1:
         necom += 1
+    if a == 0 and z == 0 and g == 0 and d == 0 and no == 0 and em == 0:
+        nonames += 1
         
 pper = (playable/count)*100
 nibper = (nibprot/count)*100
@@ -247,6 +256,7 @@ dbper = (delbric/count)*100
 nfafper = (nfaf/count)*100
 nfafbricper = (nfafbric/count)*100
 necomper = (necom/count)*100
+nonameper = (nonames/count)*100
 """
 print(eyy)
 print(playable)
@@ -254,6 +264,7 @@ print(playable)
 print("percentage playable: %d" % pper + "%" + "  %d" % playable)
 print("percentage playable through nib: %d" % nibper + "%" + "  %d" % nibprot)
 print("percentage playable with non-engine in hand: %d" % necomper + "%" + "  %d" % necom)
+print("percentage hands with zero names: %d" % nonameper + "%" + " %d" % nonames)
 """
 print("percentage bricks with delta: %d" % dbper +"%")
 print("percentage hands with 2 fafnir: %d" % nfafper +"%" + "  %d" % nfaf)
